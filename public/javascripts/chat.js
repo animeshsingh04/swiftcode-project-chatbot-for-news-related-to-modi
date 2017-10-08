aa1479366a4a2b33845272ca0c241713f4519fa2
Live session user <04animeshsing@gmail.com>
2017-10-08 04:30:44 +0000

    new chnages

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,22 @@
 
 var app = angular.module('chatApp', ['ngMaterial']);
 
-app.controller('chatController', function ($scope, $sce) {
+app.controller('chatController', function ($scope, $sce) {={=}
+
+   $scope.message={
+   'sender:USER'
+   'text':'hi'
+   'time':'10:02 AM'
+   },
+   {
+       'sender:BOT'
+       'text':'How do I help You?'
+       'time':'10:02 AM'
+   },
+   {
+    'sender:USER'
+    'text':'news about india'
+    'time':'10:02 AM'
+   }
+
 });
