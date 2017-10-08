6524b7c71aee0b8c32a8c295f7dc6e413b92c7df
Animesh <04animeshsing@gmail.com>
2017-10-08 10:45:39 +0000

    its done

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,18 @@
 var app = angular.module('chatApp', ['ngMaterial']);
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('orange', {
+            'default': '400', // by default use shade 400 from the pink palette for primary intentions
+            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+        })
+        .accentPalette('grey', {
+            'default': 'A400'
+        });
+});
+
+
 
 app.controller('chatController', function ($scope, $sce) {
 
