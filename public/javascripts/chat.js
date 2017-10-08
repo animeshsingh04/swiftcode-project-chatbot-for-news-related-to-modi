128bd8185065f916b4daa5c8db407c8b0071b795
Animesh <04animeshsing@gmail.com>
2017-10-08 07:47:40 +0000

    UI changes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,21 +2,20 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
 
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'hi',
-            'time': '10:02 AM'
-    },
-        {
-            'sender': 'BOT',
-            'text': 'How do I help You?',
-            'time': '10:02 AM'
-    },
-        {
-            'sender': 'USER',
-            'text': 'news about india',
-            'time': '10:02 AM'
-    }];
+    $scope.messages = [];
 
+    var exampleSocket = new WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = "";
+    };
+    $scope.trust = $sce.trustAsHTML;
 });
